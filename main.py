e4b9675a76a80417d11d8733cad50978ccd38d69
Atishay <v.kandarp@gmail.com>
2025-05-10 18:26:34 +0530

    Added SafeArea to ext_org.py

--- main.py
+++ main.py
@@ -4,20 +4,21 @@ import ui.img_org as img_org
 
 def main(page: ft.Page):
     # Display layout according to platform
-    platform = page.platform
+    # platform = page.platform
 
-    if platform.name.lower() in ["android", "ios"]:
-        ext_org.organize_by_ext(page)
-    elif platform.name.lower() in ["windows", "macos", "linux"]:
-        img_org.organize_by_img(page)
-    else:
-        page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
-        page.vertical_alignment = ft.MainAxisAlignment.CENTER
-        page.add(
-            ft.Text(
-                size=42.5,
-                value="Unsupported Platform"
-            )
-        )
+    # if platform.name.lower() in ["android", "ios"]:
+    #     ext_org.organize_by_ext(page)
+    # elif platform.name.lower() in ["windows", "macos", "linux"]:
+    #     img_org.organize_by_img(page)
+    # else:
+    #     page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
+    #     page.vertical_alignment = ft.MainAxisAlignment.CENTER
+    #     page.add(
+    #         ft.Text(
+    #             size=42.5,
+    #             value="Unsupported Platform"
+    #         )
+    #     )
+    ext_org.organize_by_ext()
 
 ft.app(target=main)
--- ui/ext_org.py
+++ ui/ext_org.py
@@ -1,4 +1,19 @@
 import flet as ft
 
 def organize_by_ext(page: ft.Page):
-    pass
+    page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
+    page.auto_scroll = True
+    page.add(
+        ft.SafeArea(
+            content=ft.Container(
+                bgcolor=ft.colors.BLUE_GREY_200,
+                padding=20,
+                content=ft.Column(
+                    [
+                        ft.Text("This text is inside the Safe Area.", size=20),
+                        ft.ElevatedButton("A Button", on_click=lambda _: print("Clicked!")),
+                    ]
+                ),
+            ),
+        )
+    )
